127de91746c9d7471b3d4ab0b740bee9f44802fb
Daniel González Briseño <qbixmex@gmail.com>
2024-11-04 22:23:47 -0800

    Hero Section Started
    - [x] Main HTML Elements
    - [x] Testing main elements
    - [x] Folder Structure

---
+++ tests/modules/home/components/Hero.test.tsx
@@ -0,0 +1,23 @@
+import { render, screen, within } from '@testing-library/react';
+import Hero from '@/modules/home/components/Hero';
+ 
+describe('Home Page', () => {
+  test('should render the main content', () => {
+    render(<Hero />);
+
+    const figure = within(screen.getByRole('figure'));
+    const heading = screen.getByRole('heading', {
+      level: 1,
+      name: /portfolio/i,
+    });
+    const description = screen.getByTestId("description");
+    const profileImage = figure.getByAltText("web developer", { exact: false });
+    const figCaption = figure.getByText("web developer", { exact: false });
+
+    expect(heading).toBeInTheDocument();
+    expect(description).toHaveTextContent(/web developer/i);
+    expect(profileImage).toBeInTheDocument();
+    expect(figCaption).toBeInTheDocument();
+  });
+});
+
