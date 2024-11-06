7efe54e0937afd33c69aa24e80413975e21a0f18
Daniel González Briseño <qbixmex@gmail.com>
2024-11-06 14:28:23 -0800

    Tape Section Added
    - [x] List of words
    - [x] Mobile Styles
    - [x] Tablet Styles
    - [x] Desktop Styles

--- modules/home/sections/tape/Tape.tsx
+++ modules/home/sections/tape/Tape.tsx
@@ -1,6 +1,23 @@
+import styles from "./Tape.module.css";
+import words from "@/data/tape";
+import StarIcon from "@/assets/icons/star.svg";
+
 const Tape = () => {
   return (
-    <p>Tape</p>
+    <section id={styles["tape"]}>
+      <div className={styles.background}>
+        <div className={styles.wrapper}>
+          <div className={styles.wordsList}>
+            {words.map((word) => (
+              <div key={word} className={styles.word_star}>
+                <span className={styles.word}>{word}</span>
+                <StarIcon className={styles.starIcon} />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
