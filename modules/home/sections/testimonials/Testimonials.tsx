8f79b888b7622186be5584188341bf401da41161
Daniel González Briseño <qbixmex@gmail.com>
2024-11-06 23:43:07 -0800

    Finished Testimonials Section
    - [x] Mobile Styles
    - [x] Tablet Styles
    - [x] Desktop Styles

--- modules/home/sections/testimonials/Testimonials.tsx
+++ modules/home/sections/testimonials/Testimonials.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 import testimonials from "@/data/testimonials";
 import SectionHeader from '@/modules/home/components/SectionHeader';
 
-import styles from "./Testimonials.module.css";
 import Card from "@/modules/home/components/Card";
+import styles from "./Testimonials.module.css";
 
 const Testimonials = () => {
   return (
@@ -14,24 +14,36 @@ const Testimonials = () => {
           <SectionHeader.EyeBrow>Happy Clients</SectionHeader.EyeBrow>
           <SectionHeader.Title>What Clients Say About Me</SectionHeader.Title>
           <SectionHeader.Description>
-            <>Let our <b>&ldquo;Clients&apos;s voices&rdquo;</b> illuminate the value we deliver<br />
-              and See what they say about my <b>&ldquo;Work&rdquo;</b>.</>
+            <>Let my <b>Clients&apos;s Opinions</b>
+              <br />illuminate the value I deliver and<br />
+              See what they say about my <b>Work</b>.
+            </>
           </SectionHeader.Description>
         </SectionHeader>
-
-        {testimonials.map((testimonial) => (
-          <Card key={testimonial.id}>
-            <Image
-              src={testimonial.avatar.src}
-              alt={testimonial.name}
-              width={100}
-              height={100}
-            />
-            <div>{testimonial.name}</div>
-            <div>{testimonial.position}</div>
-            <p>{testimonial.text}</p>
-          </Card>
-        ))}
+        <div className={styles.wrapper}>
+          <div className={styles.testimonialsContainer}>
+            {testimonials.map((testimonial) => (
+              <Card key={testimonial.id} className={styles.cardClasses}>
+                <div className={styles.testimonialHeader}>
+                  <figure className={styles.imageWrapper}>
+                    <Image
+                      src={testimonial.avatar.src}
+                      alt={testimonial.name}
+                      className={styles.image}
+                      width={100}
+                      height={100}
+                    />
+                  </figure>
+                  <div>
+                    <div className={styles.name}>{testimonial.name}</div>
+                    <div className={styles.position}>{testimonial.position}</div>
+                  </div>
+                </div>
+                <p className={styles.text}>{testimonial.text}</p>
+              </Card>
+            ))}
+          </div>
+        </div>
       </div>
     </section>
   );
