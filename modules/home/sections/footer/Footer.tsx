c516d3963ed01f9b06c44081ae197cd5629eb793
Daniel González Briseño <qbixmex@gmail.com>
2024-11-10 15:15:18 -0800

    Footer Finished
    - [x] Copyright
    - [x] Social Media Links
    - [x] Gradient Radial Mask

--- modules/home/sections/footer/Footer.tsx
+++ modules/home/sections/footer/Footer.tsx
@@ -1,6 +1,30 @@
+import styles from "./Footer.module.css";
+import { socialMedia } from "@/data/social-media";
+import SocialIcon from "./SocialIcon";
+
 const Footer = () => {
   return (
-    <p>Footer</p>
+    <footer id={styles["footer"]}>
+      <div className={styles.footerBackground}></div>
+      <div className="container">
+        <div className={styles.footerWrapper}>
+          <div className={styles.footerCopy}>&copy; 2024. All rights reserved.</div>
+          <nav className={styles.footerLinks}>
+            {socialMedia.map((social) => (
+              <p key={social.id}>
+                <a
+                  href={social.url}
+                  className={styles.footerLink}
+                  title={`Go to ${social.name}`}
+                >
+                  <SocialIcon icon={social.icon} />
+                </a>
+              </p>
+            ))}
+          </nav>
+        </div>
+      </div>
+    </footer >
   );
 };
 
