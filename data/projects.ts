cded8eb5754408f6086778f4016729fee7539767
Daniel González Briseño <qbixmex@gmail.com>
2024-11-13 14:45:00 -0800

    Projects Improvement
    - Description added
    - Tools with icons added

--- data/projects.ts
+++ data/projects.ts
@@ -1,7 +1,12 @@
 import { StaticImageData } from "next/image";
-import homega_records from "@/assets/images/projects/homega-records.jpg";
+import omega_records from "@/assets/images/projects/homega-records.jpg";
 import game_hub from "@/assets/images/projects/game-hub.jpg";
 import tesloShop from "@/assets/images/projects/teslo-shop-2024.jpg";
+import { type IconType } from "react-icons";
+import { FaReact, FaHtml5, FaCss3, FaBootstrap, FaPaypal } from "react-icons/fa6";
+import { SiTypescript, SiCloudinary, SiPrisma, SiVite } from "react-icons/si";
+import { RiNextjsFill, RiJavascriptFill, RiTailwindCssFill } from "react-icons/ri";
+import { BiLogoPostgresql } from "react-icons/bi";
 
 interface Project {
   id: string;
@@ -10,25 +15,29 @@ interface Project {
   title: string;
   url: string;
   image: StaticImageData;
-  results: Result[];
+  description: string;
+  tools: Tool[];
 };
 
-interface Result {
-  title: string;
+interface Tool {
+  name: string;
+  icon: IconType;
 };
 
 const portfolioProjects: Project[] = [
   {
     id: "e9bd8940-e123-4d1e-85af-fd1a8688bfb6",
-    company: "Homega Records",
+    company: "Omega Records",
     year: "2020",
     title: "Recorgins Studio Web Site",
     url: "https://qbixmex.github.io/omega-records/",
-    image: homega_records,
-    results: [
-      { title: "Enhanced user experience by 40%" },
-      { title: "Improved site speed by 50%" },
-      { title: "Increased mobile traffic by 35%" },
+    image: omega_records,
+    description: "<p>This website showcases a Recording Studio, highlighting its services, state-of-the-art equipment, experienced team, available courses, and a contact page for inquiries.</p><p>It combines a clean design with responsive functionality to deliver an engaging user experience.</p>",
+    tools: [
+      { name: "HTML", icon: FaHtml5 },
+      { name: "CSS", icon: FaCss3 },
+      { name: "Bootstrap", icon: FaBootstrap },
+      { name: "Javascript", icon: RiJavascriptFill },
     ],
   },
   {
@@ -38,10 +47,11 @@ const portfolioProjects: Project[] = [
     title: "Video Games Platform",
     url: "https://game-hub-qbixmex.vercel.app",
     image: game_hub,
-    results: [
-      { title: "Boosted sales by 40%" },
-      { title: "Expanded customer reach by 50%" },
-      { title: "Increased brand awareness by 35%" },
+    description: "<p>This React-based website lets users explore a curated selection of video games, offering powerful filters by platform, genre, and relevance and search functionality.</p><p>Each game has a detailed view, showcasing its description, Metascore, publisher information, and gameplay images for an immersive browsing experience.</p>",
+    tools: [
+      { name: "Vite", icon: SiVite },
+      { name: "React", icon: FaReact },
+      { name: "Typescript", icon: SiTypescript },
     ],
   },
   {
@@ -51,10 +61,15 @@ const portfolioProjects: Project[] = [
     title: "E-commerce Platform",
     url: "https://teslo-shop-2024-green.vercel.app/",
     image: tesloShop,
-    results: [
-      { title: "Enhanced user experience by 40%" },
-      { title: "Improved site speed by 50%" },
-      { title: "Increased traffic by 35%" },
+    description: "<p>This full-stack shopping cart website features a complete online clothing store for Men, Women, and Kids, with integrated database support, product image uploads, and PayPal payment processing.</p><p>Users can filter clothing by category, while an admin panel allows for seamless product management and updates.</p>",
+    tools: [
+      { name: "Next.js", icon: RiNextjsFill },
+      { name: "Typescript", icon: SiTypescript },
+      { name: "Tailwind", icon: RiTailwindCssFill },
+      { name: "Prisma", icon: SiPrisma },
+      { name: "Postgresql", icon: BiLogoPostgresql },
+      { name: "Cloudinary", icon: SiCloudinary },
+      { name: "Paypal", icon: FaPaypal },
     ],
   },
 ] as const;
